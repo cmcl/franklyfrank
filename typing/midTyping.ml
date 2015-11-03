0c8ce11f3e7e804b1580a5b0c48775951b28c723
Craig McLaughlin <mr_mcl@live.co.uk>
2015-11-03 18:20:56 +0000

    Refactored coverage code (not complete).

--- typing/midTyping.ml
+++ typing/midTyping.ml
@@ -302,7 +302,8 @@ and type_ccomp env res cc =
 	      (ShowMidCValue.show cv) (ShowSrcType.show res))
        end
   | Mccomp_clauses [], _ -> type_empty_clause env res
-  | Mccomp_clauses cls, _ -> type_clauses env res cls
+  | Mccomp_clauses cls, _ -> let _ = covers env cc res in
+			     type_clauses env res cls
   | _ , _
     -> type_error (Printf.sprintf
 		     "failed to typecheck checkable computation %s against %s"
@@ -316,17 +317,16 @@ and destruct_comp_type t =
 				   (ShowSrcType.show (uniq_type t)))
 
 and type_empty_clause env res =
+  let validate_arg t =
+    match t.styp_desc with
+    | Styp_ret (es, v)
+      -> if is_uninhabited env v then ()
+	 else let msg = Printf.sprintf "%s not uninhabited"
+		(ShowSrcType.show t) in
+	      type_error msg
+    | _ -> type_error "expected returner type for empty clause" in
   match res.styp_desc with
-  | Styp_comp ([t], r)
-    -> begin
-         match t.styp_desc with
-	 | Styp_ret (es, v)
-	   -> if is_uninhabited env v then res
-    	      else let msg = Printf.sprintf "%s not uninhabited"
-		     (ShowSrcType.show v) in
-		   type_error msg
-	 | _ -> type_error "expected returner type for empty clause"
-       end
+  | Styp_comp (ts, r) -> List.iter validate_arg ts; res
   | Styp_ref pt -> begin
                      match (unbox res).styp_desc with
 		     | Styp_ftvar _
@@ -482,19 +482,26 @@ and type_value_pattern env (t, vp) =
   | _ , _ -> pattern_error t (Pattern.vpat vp) (* Shouldn't happen *)
 
 and covers env e t =
+  let error cc t =
+    let msg = Printf.sprintf "%s does not cover %s"
+      (ShowMidCComp.show cc) (ShowSrcType.show t) in
+    type_error msg in
   let rec extract_patterns cls =
     match cls with
     | [] -> []
     | (ps, _) :: cls -> ps :: extract_patterns cls in
+  let is_valid_pattern t r p = true in
   (* Coverage checking for one argument with early exit checking. *)
-  let covers' r (t, ps) = true in
+  let covers' r (t, ps) =
+    if not r then false
+    else foldl (is_valid_pattern t) false ps in
   match (unbox t).styp_desc, e with
   | Styp_comp (ts, r), Mccomp_clauses cls
     -> let pss = transpose (extract_patterns cls) in
-       Debug.print "COVERS: %d (%s) vs. %d\n" (length ts) (show_types ts)
-	 (length pss);
-       true
-  |  _ -> true
+       if length ts != length pss then error e t
+       else (Debug.print "COVERS: %d (%s) vs. %d\n" (length ts)
+	       (show_types ts) (length pss); foldl covers' true (zip ts pss))
+  |  _ , _ -> true
 
 and type_cvalue env res cv =
   match cv, res.styp_desc with
@@ -507,13 +514,7 @@ and type_cvalue env res cv =
        unify res t
   | Mcvalue_ctr (k, vs), Styp_datatype (d, ts) -> type_ctr env (k, vs) (d, ts)
   | Mcvalue_thunk cc, Styp_thunk c
-    -> if covers env cc c then
-        (Debug.print "CHECKING THUNK\n";
-	 TypExp.sus_comp (type_ccomp env c cc))
-       else (Debug.print "FALSE\n";
-            let msg = Printf.sprintf "%s does not cover %s"
-	      (ShowMidCComp.show cc) (ShowSrcType.show c) in
-	    type_error msg)
+    -> Debug.print "CHECKING THUNK\n"; TypExp.sus_comp (type_ccomp env c cc)
   | _ , Styp_ref pt
     -> begin
          match cv, (unbox res).styp_desc with
@@ -675,6 +676,10 @@ and get_interface t =
   | _ -> assert false
 
 and sub es es' =
+  let error es es' =
+    let msg = Printf.sprintf "failed sub: %s <= %s"
+      (show_types es) (show_types es') in
+    type_error msg in
   let is_var t s =
     match (unbox t).styp_desc with
     | Styp_rtvar (s', _) when s = s' -> true
@@ -686,14 +691,16 @@ and sub es es' =
   let eq r (t, s) = r && eq' t s in
   match es with
   | [t] when is_empty_set t -> true
-  | t :: _ when is_effect_var t -> foldl eq true (zip es es')
+  | t :: _ when is_effect_var t
+    -> begin
+         try foldl eq true (zip es es') with
+         | Invalid_argument _ -> error es es'
+       end
   | t :: es when is_interface t -> let ei = get_interface t in
 				   let es' = remove_first es' ei in
 				   sub es es'
   | _ -> if unify_effect_sets es es' then true
-         else let msg = Printf.sprintf "failed sub: %s <= %s"
-		(show_types es) (show_types es') in
-	      type_error msg
+         else error es es'
 
 and remove_first es' ei =
   match es' with
